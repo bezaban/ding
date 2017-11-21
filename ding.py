cb541a454f846074ce9bf4078de1d3ef39c71277
bezaban <2104926+bezaban@users.noreply.github.com>
2017-11-21 21:01:27 +0100

    Moved to BaseHTTPServer

--- ding.py
+++ ding.py
@@ -1,28 +1,60 @@
 #!/usr/bin/python
 
-import SimpleHTTPServer
 import SocketServer
+import BaseHTTPServer
 import ConfigParser
 import argparse
 import os
 import sys
 import logging
 
-class LoggingHttpHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
-    def log_message(self, format, *args):
-        logging.info("%s - - [%s] %s\n" %(self.client_address[0], self.log_date_time_string(), format%args))
+class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
-    def version_string(self):
-        return("Hackeriet Doorbell API")  
+    def do_POST(self):
+        if not self.validate_post_request():
+            return
+
+    def log_message(self, format, *args):
+        self.server_version = "Hackeriet doorbell"
+        self.sys_version = ""
+        logging.info("%s - - [%s] %s" %(self.client_address[0], self.log_date_time_string(), format%args))
+
+    def validate_post_request(self):
+        if self.path != "/":
+            self.send_error(404)
+            return False
+ 
+        if self.headers["content-type"] != "application/json":
+            self.send_error(400, "Only accepts application/json")
+            return False
+ 
+        if not "content-length" in self.headers:
+            self.send_error(400, "No content length set")
+            return False
+   
+        content_length = int(self.headers["content-length"])
+ 
+        # Valid JSON contains at least "{}"
+        if content_length < 2:
+            self.send_error(400, "Post data too short")
+            return False
+ 
+        if content_length > MAX_REQUEST_LENGTH:
+            self.send_error(400, "Request too big")
+            return False
+        
+        # Trust that the request is legit if we're here
+            return True
 
 def runHttpServer(listen_port):
-    Handler = LoggingHttpHandler 
+
+    Handler = RequestHandler   
     httpd = SocketServer.TCPServer(("", listen_port), Handler)
     logging.info("Server started on port tcp/%s", listen_port)
     return httpd
 
 def main():
-    version = "0.1"
+    version = "0.2"
 
     # parse cmdline arguments
     parser = argparse.ArgumentParser()
@@ -61,8 +93,20 @@ def main():
         print >> sys.stderr, "ERROR. %s" % error
         sys.exit(1)
 
-    server = runHttpServer(listen_port)
-    server.serve_forever()
+    try: 
+        server = runHttpServer(listen_port)
+        server.serve_forever()
+
+
+    except Exception as error: 
+        print >> sys.stderr, "ERROR. %s" % error
+        if (str(args.debug)): logging.debug("Exception raised: %s", error)
+        sys.exit(1)
+    except KeyboardInterrupt:
+        logging.info("Caught ^C")
+        print("\nProgram exit")
+    finally:
+        logging.info("Shutting down")
 
 if __name__ == "__main__":
         main()
