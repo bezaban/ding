7b72dfd3fc5f98335e19b1d4668ec52a983333c9
bezaban <2104926+bezaban@users.noreply.github.com>
2017-12-19 20:43:26 +0100

    added git, libespeak-dev

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 # Required for pyaudio
-sudo apt-get install python-pip python-dev portaudio19-dev
+sudo apt-get install python-pip python-dev portaudio19-dev libespeak-dev git
 # text to speech
 pip install --user pyttsx
 # play wav
